de4ffb255221c3ee0e66c1bc423631b29a5edd52
ajbert <100375590+ajbert@users.noreply.github.com>
2022-09-21 21:39:22 -0500

    Add files via upload

---
+++ Models/Cow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractExample.Models
+{
+    public class Cow : Animal
+    {
+        public Cow()
+        {
+            Name = "Elsie";
+        }
+        public override void MakeNoise()
+        {
+            Console.WriteLine($"{Name} says Moo!");
+        }
+
+        public override void Sleep()
+        {
+            Console.WriteLine($"{Name} snores!");
+        }
+    }
+}
